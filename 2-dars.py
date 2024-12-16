f0c2911c29bc21ba91a56f345d1590e6ee65e17d
Muhammadqodir <alekscraft16@gmail.com>
2024-12-16 16:43:07 +0300

    2-dars
    2-dars yuklandi

---
+++ 2-dars.py
@@ -0,0 +1,104 @@
+# a = float(input("Soni kriting: "))
+# if a == 0:
+#      print("Musbat va manfiy emas 0!")
+# elif  a>0:
+#      print("Musbat")
+# else:
+#     print("Manfiy")
+
+
+# a = float(input("Son kiriting: "))
+# if a % 2 == 0:
+#      print("Juft")
+# if a % 2 == 1:
+#      print("Toq")
+
+
+
+# a = int(input("Son kiriting: "))
+# if a % 3 == 0 and a % 7 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+
+# a = float(input("1-Soni kiriting: "))
+# b = float(input("2-Soni kiriting: "))
+# c = float(input("3-Soni kiriting: "))
+# s = 0
+# d = 0
+# if a > 0:
+#     s = s + 1
+# elif a < 0:
+#      d = d + 1
+# if b > 0:
+#     s = s + 1
+# elif b < 0:
+#      d = d + 1
+# if c > 0:
+#     s = s + 1
+# elif c < 0:
+#      d = d + 1
+# print(s)
+# print(d)
+
+
+# a = float(input("1-Soni kiriting: "))
+# b = float(input("2-Soni kiriting: "))
+# c = float(input("3-Soni kiriting: "))
+# s = 0
+# d = 0
+# f = 0
+# if a > 0:
+#     s = s + 1
+# elif a < 0:
+#      d = d + 1
+# else:
+#     f += 0 
+# if b > 0:
+#     s = s + 1
+# elif b < 0:
+#      d = d + 1
+# else:
+#     f += 0      
+# if c > 0:
+#     s = s + 1
+# elif c < 0:
+#      d = d + 1
+# else:
+#     f += 0
+
+# if s > 0:
+#     print(f"{s} ta musbat")
+# if d > 0:
+#     print(f"{d} ta manfiy")
+# if f > 0:
+#     print(f"{f} ta 0 bor")
+
+
+# a = int(input("Narhni kiriting: "))
+# if a > 0 and a <= 1_000_000:
+#     if a >= 500:
+#         print(a*0.7)
+#     elif a >= 300:
+#         print(a*0.8)
+#     elif a >= 100:
+#         print(a*0.9)
+#     elif a > 0:
+#         print(a)
+# else:
+#     print("Xatolik")
+
+
+
+
+
+# a = int(input("1-sonni kiriting"))
+# b = int(input("2-sonni kiriting"))
+# c = int(input("3-sonni kiriting"))
+# if a > b:
+#     print(a)
+# elif b > c:
+#     print(b)
+# elif c > a:
+#     print(c)
